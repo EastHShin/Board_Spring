b0f651e08f328cd2c657906317257613351ca28b
EastHShin <xldk78@gmail.com>
2021-08-28 23:05:07 +0900

    page처리

--- src/main/java/com/board/easth/Controller/BoardController.java
+++ src/main/java/com/board/easth/Controller/BoardController.java
@@ -4,6 +4,10 @@ import com.board.easth.model.Board;
 import com.board.easth.model.repository.BoardRepository;
 import com.board.easth.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -23,8 +27,13 @@ public class BoardController {
     private BoardValidator  boardValidator;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Board> boards = boardRepository.findAll();
+    public String list(Model model, @PageableDefault(size = 1) Pageable pageable) {
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int currentPage = boards.getPageable().getPageNumber();
+        int startPage = (currentPage - (currentPage % 5)) + 1;
+        int endPage = Math.min(boards.getTotalPages(),startPage + 4);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         model.addAttribute("boards", boards);
         return "board/list";
     }
